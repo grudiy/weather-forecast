4c27c73401dc604c402187ee2e85a44c1f585757
gruand <gruand@gmail.com>
2024-03-02 17:16:41 +0200

    Parse temperature from API on backend

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import plotly.express as px
+from backend import get_data
 
 st.title("Weather Forecast App")
 place = st.text_input("Place:")
@@ -8,12 +9,7 @@ days = st.slider("Forecast for Days: ", min_value=1, max_value=5,
 option = st.selectbox("Select data to view", ("Temperature", "Sky"))
 st.subheader(f"{option} for the next {days} days in {place}")
 
-
-def get_data(days_period):
-    dates = ["2023-10-25", "2023-10-26", "2023-10-30"]
-    temperatures = ["10", "11", "-5"]
-    return dates, temperatures
-
+data = get_data(place, days, option)
 
 d, t = get_data(days)
 figure = px.line(x=d, y=t, labels={"x": "Date", "y": "Temperature"})
